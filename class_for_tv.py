1fcec45766f4c83e551233b77e6f0eec0abd7e2c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-19 09:45:13 +0800

    Done adding the instance methods.

--- class_for_tv.py
+++ class_for_tv.py
@@ -7,10 +7,32 @@
 class TV:
     # - Constructor
     def __init__(self, channel, volume_level, on):
-        
-# - Instance variables
+    # - Instance variables
         self.channel = channel
         self.volume_level = volume_level
         self.on = on
+    
+    def turn_on(): None
+
+    def turn_off(): None
+
+    def get_channel(): int
+    
+    def set_channel(channel: int): None
+    
+    def get_volume(): int
+    
+    def set_volume(volume_level: int): None
+    
+    def channel_up(): None
+    
+    def channel_down(): None
+    
+    def volume_up(): None
+    
+    def volume_down(): None
+    
+    
+    
 
 # - Instance methods
