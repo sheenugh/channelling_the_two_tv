6771b862d83728fec30cbc3bbf83adbf95387d25
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-20 00:16:48 +0800

    Adding code for instance methods channel up, channel down, volume up, volume down, and adding comment to them to comprehend their purpose.

--- class_for_tv.py
+++ class_for_tv.py
@@ -21,26 +21,36 @@ class TV:
     def get_channel(self):
         return self.channel # - Setting in this kind of approach to access the set channel value in the instance variable. Simply , it will return to the default channel of the TV.
     
-    def set_channel(self, channel): # - Putting the 'channel' para maaccess at madetermine ng if else ang inputted int doon sa object parameter.
+    def set_channel(self, channel): # - Putting the 'channel' para maaccess at madetermine ng if else ang sinet na int doon sa object parameter.
         if 1<= channel <= 120:
-            self.channel = channel # - Setting this kind of approach will automatically produce an output that is equal to the number that has been set to the tv's channel.
+            self.channel = channel # - Setting this kind of approach will automatically produce an output that is equal to the number that has been set to the tv's channel in the test driver program.
         else: 
             raise ValueError("The channel must be between 1 to 120. Please input a number between this number.")
     
     def get_volume(self):
         return self.volume_level # - Gets or return the volume of the TV, where volume = 1.
     
-    def set_volume(self, volume_level): 
+    def set_volume(self, volume_level): # - Putting the 'volume_level' data member to connect on what integer has been put for the tv's volume in the test driver program.
         if 1<= volume_level <=7:
-            self.volume_level = volume_level
+            self.volume_level = volume_level # - self.volume_level will now equal to the value that has been set on the test driver program.
+        else:
+            raise ValueError("Error. Please input a number between 1 to 7.")
     
-    def channel_up(): None
+    def channel_up(self): 
+        if self.channel < 120:
+            self.channel += 1 # - Setting this because if ever the set channel is above 120, then it will still equal to the default value.
     
-    def channel_down(): None
+    def channel_down(self): 
+        if self.channel > 1:
+            self.channel -= 1 # - Setting this because if ever the set channel is below 1, then it will still equal to the default value.
     
-    def volume_up(): None
+    def volume_up(self): 
+        if self.volume_level < 7:
+            self.volume_level += 1 # - Setting this because if ever the set volume level is above 7, then it will still equal to the default value.
     
-    def volume_down(): None
+    def volume_down(self): 
+        if self.volume_level > 1:
+            self.volume_level -= 1 # - Setting this because if ever the set volume level is below 7, then it will still equal to the default value.
     
     
     
