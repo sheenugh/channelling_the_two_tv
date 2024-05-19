0ddd356d0b70aa4573cc238b18f83501473db4a0
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-19 23:43:43 +0800

    Adding code for the instance method get_channel and set_channel.

--- class_for_tv.py
+++ class_for_tv.py
@@ -12,14 +12,20 @@ class TV:
         self.volume_level = volume_level
         self.on = on
     
-    def turn_on():
+    def turn_on(self):
         self.on = True # - Making the "turning on" of the TV as "True" or equivalent to "True."
 
-    def turn_off():
+    def turn_off(self):
         self.on = False # - Making the "turning off" of the TV as "False" or equivalent to "False."
-    def get_channel(): int
     
-    def set_channel(channel: int): None
+    def get_channel(self):
+        return self.channel # - Setting in this kind of approach to access the set channel value in the instance variable.
+    
+    def set_channel(self, channel): # - Putting the 'channel' para maaccess at madetermine ng if else ang inputted int doon sa object parameter.
+        if 1<= channel <= 120:
+            self.channel = channel
+        else: 
+            raise ValueError("The channel must be between 1 to 120. Please input a number between this number.")
     
     def get_volume(): int
     
