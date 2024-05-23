99969ee73e6adf911a5269ca5adae1e7d2ea3dd4
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 21:20:59 +0800

    NEW AND RECENT FILE NAME FOR TEST DRIVER | Adding PSEUDO CODE.

---
+++ test_driver_for_the_two_tv_channel.py
@@ -0,0 +1,12 @@
+# TEST DRIVER PROGRAM FOR TESTING THE TV
+
+# ========== PSEUDO CODE =========
+# || IMPORTS ||
+
+
+# || PACKAGES/LIBRARY ||
+
+
+# || ACTUAL CODES || 
+# - TV 1 and TV 2 objects together with the def function
+# - Calling for the def function
