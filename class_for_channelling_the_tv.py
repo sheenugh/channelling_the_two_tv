0df8115a83bf0f9f680d26a6a494e57125579f8e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 21:30:38 +0800

    NEW AND RECENT PYTHON FILE OF THE 'CLASS FOR CHANNELLING THE TV'| Adding the constructor and instance variables.

--- class_for_channelling_the_tv.py
+++ class_for_channelling_the_tv.py
@@ -2,12 +2,23 @@
 
 # ========== PSEUDO CODE =========
 # || IMPORTS ||
+
+
 # || ACTUAL CODES || 
+
+
 # - Class name.
-# - Constructor.
-# - Instance Variables.
-# - Instance Methods.
-# - Instance Method: Turning on the TV.
+class TV:
+    # - Constructor.
+    def __init__(self, channel = 1, volume_level = 1, on = False):
+        # - Instance Variables.
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on = on
+    
+    # - Instance Methods.
+    # - Instance Method: Turning on the TV.
+    
 # - Instance Method: Turning off the TV.
 # - Instance Method: Get the channel.
 # - Instance Method: Set the channel.
