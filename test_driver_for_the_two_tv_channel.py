6978f7bb0269a41d350e5d55c1d5d1cd670aca7c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-24 00:32:43 +0800

    NEW AND RECENT PYTHON FILE OF THE 'TEST DRIVER'| Deleting the previous code and changing it to make the output in the tkinter window.

--- test_driver_for_the_two_tv_channel.py
+++ test_driver_for_the_two_tv_channel.py
@@ -11,22 +11,12 @@ from class_for_channelling_the_tv import TV
 # || ACTUAL CODES || 
 # - Class name 
 class TestTV:
-    # - TV 1 and TV 2 objects together with the def function
-    def channelling_tv1_and_tv2(self):
-        tv1 = TV()
-        tv1.turn_on()
-        tv1.set_channel(30)
-        tv1.set_volume(3)
-
-        tv2 = TV()
-        tv2.turn_on()
-        tv2.set_channel(3)
-        tv2.set_volume(2)
-        
-        print(f"tv1 channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
-        print(f"tv2 channel is {tv2.get_channel()} and volume level is {tv2.get_volume()}")
-        
-        
-if __name__ == "__main__":
-    test_tv = TestTV()
-    test_tv.channelling_tv1_and_tv2()
+    # - Constructor.
+    # - Using the try and except for the tkinter window for the purpose of to appear or destroy if the channel will be changed in the range of 1>channel<120 and the volume 1>volume<7.
+    # - Instance variables.
+    # - TV Frame.
+    # - Load TV image.
+    # - Channel control buttons
+    # - Volume control buttons
+    # - Display the current channel
+    # - Operator
