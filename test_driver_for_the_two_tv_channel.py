7122f949fd2757d31bbe46e8832ffaa2743a0973
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 22:43:32 +0800

    NEW AND RECENT PYTHON FILE OF THE 'TEST DRIVER'| Adding the code for class, def and code to run or gumana yong def.

--- test_driver_for_the_two_tv_channel.py
+++ test_driver_for_the_two_tv_channel.py
@@ -2,11 +2,21 @@
 
 # ========== PSEUDO CODE =========
 # || IMPORTS ||
+from class_for_channelling_the_tv import TV
 
 
 # || PACKAGES/LIBRARY ||
 
 
 # || ACTUAL CODES || 
-# - TV 1 and TV 2 objects together with the def function
-# - Calling for the def function
+# - Class name 
+class TestTV:
+    # - TV 1 and TV 2 objects together with the def function
+    def channelling_tv1_and_tv2(self):
+        tv1 = TV()
+        
+
+
+if __name__ == "__test_driver_for_the_two_tv_channel__":
+    test_tv = TestTV()
+    test_tv.test_driver_for_the_two_tv_channel()
