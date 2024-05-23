8ca3c7b0336c60f4c1471ef3009e3196ec3dd226
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 21:34:22 +0800

    NEW AND RECENT PYTHON FILE OF THE 'CLASS FOR CHANNELLING THE TV'|  Adding code of the instance methods get channe and set channel.

--- class_for_channelling_the_tv.py
+++ class_for_channelling_the_tv.py
@@ -25,11 +25,20 @@ class TV:
     def turn_off(self):
         self.on = False # - Making the "turning off" of the TV as "False" or equivalent to "False."
 
-# - Instance Method: Get the channel.
-# - Instance Method: Set the channel.
-# - Instance Method: Get the volume.
-# - Instance Method: Set the volume.
-# - Instance Method: Channel up.
-# - Instance Method: Channel down.
-# - Instance Method: Volume up.
-# - Instance Method: Volume down.
+    # - Instance Method: Get the channel.
+    def get_channel(self):
+        return self.channel # - Setting in this kind of approach to access the set channel value in the instance variable. Simply , it will return to the default channel of the TV.
+    
+    # - Instance Method: Set the channel.
+    def set_channel(self, channel): # - Putting the 'channel' para maaccess at madetermine ng if else ang sinet na int doon sa object parameter.
+        if 1<= channel <= 120:
+            self.channel = channel # - Setting this kind of approach will automatically produce an output that is equal to the number that has been set to the tv's channel in the test driver program.
+        else: 
+            raise ValueError("The channel must be between 1 to 120. Please input a number between this number.")
+        
+    # - Instance Method: Get the volume.
+    # - Instance Method: Set the volume.
+    # - Instance Method: Channel up.
+    # - Instance Method: Channel down.
+    # - Instance Method: Volume up.
+    # - Instance Method: Volume down.
