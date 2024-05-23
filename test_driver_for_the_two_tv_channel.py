55c1393e91018b0d4fc5032a4f3f30d061d2ea8e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 22:47:03 +0800

    NEW AND RECENT PYTHON FILE OF THE 'TEST DRIVER'| Added code for object 1 = tv1 and object 2 = tv2.

--- test_driver_for_the_two_tv_channel.py
+++ test_driver_for_the_two_tv_channel.py
@@ -14,6 +14,15 @@ class TestTV:
     # - TV 1 and TV 2 objects together with the def function
     def channelling_tv1_and_tv2(self):
         tv1 = TV()
+        tv1.turn_on()
+        tv1.set_channel(30)
+        tv1.set_volume(3)
+
+        tv2 = TV()
+        tv2.turn_on()
+        tv2.set_channel(3)
+        tv2.set_volume(2)
+        
         
 
 
