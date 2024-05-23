c45e488068368eb86c1ae3b9125d0d10b9e45c99
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-23 21:27:57 +0800

    NEW AND RECENT PYTHON FILE OF THE 'CLASS FOR CHANNELLING THE TV': Adding PSEUDO CODE.

---
+++ class_for_channelling_the_tv.py
@@ -0,0 +1,19 @@
+# CLASS CATEGORY FOR TESTING THE TV
+
+# ========== PSEUDO CODE =========
+# || IMPORTS ||
+# || ACTUAL CODES || 
+# - Class name.
+# - Constructor.
+# - Instance Variables.
+# - Instance Methods.
+# - Instance Method: Turning on the TV.
+# - Instance Method: Turning off the TV.
+# - Instance Method: Get the channel.
+# - Instance Method: Set the channel.
+# - Instance Method: Get the volume.
+# - Instance Method: Set the volume.
+# - Instance Method: Channel up.
+# - Instance Method: Channel down.
+# - Instance Method: Volume up.
+# - Instance Method: Volume down.
